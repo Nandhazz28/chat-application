abd7b0f0ca785a0a06cc232e455f6d7f7f0bf008
Nandha <nandabalan35@gmail.com>
2026-06-30 21:47:24 +0530

    deployment

--- Server/app.js
+++ Server/app.js
@@ -18,7 +18,7 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "https://opentunnel.netlify.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
